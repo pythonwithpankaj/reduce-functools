ceec2c07433e584a669104685bead2f27235c41f
Pankaj Kumar <137532694+pythonwithpankaj@users.noreply.github.com>
2023-08-22 13:34:00 +0530

    filter_reduce functools
    reduce

---
+++ map_filter_reduce.py
@@ -0,0 +1,79 @@
+# res=functools.reduce(Iambda a,b:a*b,i1)
+# print(res)
+
+ # n=43545
+ # n=n//10 == 4354
+ # n=n//10 == 435
+ # n=n//10 == 43
+ # n=n//10 == 4
+ # n=n//10 == 0
+ # print(len(str(n)))
+ # s=0
+ # rev=0
+ # while n>0:
+ #     d=n%10
+ #     rev=rev*10+d
+ #     n//=10
+ # print(rev)    
+ # a b c
+ # 0,1,1,2,3,5,8,13 
+
+
+ # a=0
+ # b=1
+ # print(a,b,end=" ")
+ # 
+ # while True:
+ #     c=a+b
+ #     if c>50:
+ #         break
+ #     print(c,end=" ")
+ #     a=b
+ #     b=c
+
+ ## word="python"
+ ## rev_word=""
+ ## i=len(word)-1
+ ## while i>=0:
+ ##     rev_word=rev_word+word[i]
+ ##     i-=1
+ ## print(rev_word)   
+
+ # import random
+  
+ # n=random.randint(1,9)
+ # i=1
+ # while i<=5:
+ #     g=int(input("guess a number between 1-9:"))
+ #     if g==n:
+ #         print("well guessed!!!")
+ #         break
+ #     else:
+ #         print("your",5-i,"chance are left")
+ #     i+=1
+ # else:
+ #     print("you loose the game:(")
+
+
+##character(abcd)
+ # i=65
+ # while i<=90:
+ #     print(chr(i),end=" ")
+ #     i+=1 
+
+ # print(ord('A'))       
+
+ # *
+ # **
+ # ***
+ # ****
+ # *****
+
+ # row=1
+ # while row<=5:
+ #     col=1
+ #     while col<=row:
+ #         print('*',end=" ")
+ #         col+=1
+ #     print()
+ #     row+=1      
